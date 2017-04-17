f425a6166b3d45e97eba3bb45743bd71dd8785a0
Dilhanx <dilhanx@outlook.com>
2017-04-17 17:44:32 +0530

    Signed-off-by: Dilhanx <dilhanx@outlook.com>

---
+++ app/src/main/java/entites/creature.java
@@ -0,0 +1,24 @@
+package entites;
+
+/**
+ * Created by dilhan.
+ */
+
+public class creature {
+    int creatureID ;
+    int maxhp;
+    int maxmp;
+    int hp;
+    int mp;
+    int str;
+    int dex;
+    int intel;
+    int tempstr;
+    int tempdex;
+    int tempintel;
+    int ac;
+    String name;
+
+
+
+}
